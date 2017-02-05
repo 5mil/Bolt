d3b884d30d9744e28463af12b9266e951aa78b6e
Joey-PC\Joey <5mil@tuta.io>
2017-02-05 15:16:13 -0500

    Changing stake age settings

--- src/main.h
+++ src/main.h
@@ -43,8 +43,8 @@ static const int COINBASE_MATURITY_PPC = 100;
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
 static const int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
 static const int STAKE_TARGET_SPACING = 2.5 * 60; // 2.5-minute block spacing
-static const int STAKE_MIN_AGE = 60 * 60 * 6 * 1; // minimum age for coin age 2 days
-static const int STAKE_MAX_AGE = 60 * 60 * 24 * 365 * 6; // stake age of full weight 6 years
+static const int STAKE_MIN_AGE = 60 * 60 * 6 * 1; // minimum age for coin age 6 hours
+static const int STAKE_MAX_AGE = 60 * 60 * 24 * 365 * 1; // stake age of full weight 1 years
 
 #ifdef USE_UPNP
 static const int fHaveUPnP = true;
